e62e7fe5ede0ffc88b1f1881713e37310b11a4ea
Helmut <helmut@Helmuts-MacBook-Pro.local>
2020-05-15 11:01:40 +0200

    Add application and MC simulations

---
+++ Application/AE98.R
@@ -0,0 +1,70 @@
+#################################################
+# Application in
+# "Instrument Validity Tests with Causal Forests"
+# Helmut Farbmacher et al 2020
+# May 2020
+#################################################
+
+rm(list=ls())
+packages <- c("LATEtest","readstata13", "dummies")
+invisible(lapply(packages, library, character.only=T))
+
+system <- Sys.info()['sysname']
+if(system == 'Windows') {
+  setwd("C:/Users/farbma/Documents/GitHub/LATEtest")    # my path Windows
+} else if(system == 'Darwin') {
+  setwd("/Users/helmut/Documents/GitHub/LATEtest")      # my path Mac
+}
+
+set.seed(10101)
+setwd("./Application")
+data<-read.dta13("AE98_data.dta")
+nrow(data)
+
+subsets <- 4
+synthetic <- TRUE     # adds synthetic direct effects to the outcome (empirical Monte-Carlo simulation)
+
+Xvars <- paste0(c("age","agefst","othrace","white","black","hisp","lesshs","hs","morehs"))
+data$Y <- data$lincome
+data$D <- data$morekids
+data$Z <- data$samesex
+
+if(synthetic==TRUE) {
+  subset <- data$age<25 & data$hs==1
+  table(data$hs,data$age)
+  table(subset(data.frame(data$hs,data$age),subset))
+  table(subset(data.frame(data$Z,data$D),subset))
+  print("mean(data$Y)")
+  print(mean(data$Y))
+  print("sd(data$Y)")
+  print(sd(data$Y))
+  print("length(subset(data$Y,subset))")
+  print(length(subset(data$Y,subset)))
+  print("length(subset(data$Y,subset))/nrow(data)")
+  print(length(subset(data$Y,subset))/nrow(data))
+  print("mean(subset(data$Y,(subset)))")
+  print(mean(subset(data$Y,(subset))))
+  effect_size <- 1/4*sd(data$Y)
+  print("effect_size")
+  print(effect_size)
+  data$Y <- data$Y + effect_size*subset*data$Z
+  print("mean(subset(data$Y,subset))")
+  print(mean(subset(data$Y,subset)))
+  print("mean(data$Y)")
+  print(mean(data$Y))
+}
+
+start_time = Sys.time()
+test <- LATEtest(data=data, covars=Xvars, subsets=subsets, huge=TRUE, tree_fraction=0.05, alpha=0.05, minsize=800)
+end_time = Sys.time()
+maxtree <- eval(parse(text=paste("test$treelist$tree_",test$maxTtree$label,test$maxTtree$J,sep="")))
+rpart.plot(maxtree,extra = 101,box.palette="GyRd",shadow.col="gray",nn=TRUE,roundint=FALSE)
+
+sink(file=paste("AE98_FGK_synth_",synthetic,".txt",sep=""), split=TRUE)
+print(Sys.time())
+end_time - start_time
+print("test$leafinfo")
+test$leafinfo
+print("test$results")
+test$results
+sink(file=NULL)
--- Application/FGK_MCsimulations.R
+++ Application/FGK_MCsimulations.R
@@ -6,13 +6,60 @@
 #################################################
 
 rm(list=ls())
-#p.latetest <- c("grf", "rlearner", "rpart", "treeClust")
 packages <- c("LATEtest","foreach", "doParallel", "mvtnorm")
 invisible(lapply(packages, library, character.only = TRUE))
 
-setwd("/Users/helmut/Documents/GitHub/LATEtest/Simulations")   # set working directory path
-source("dgp_setup.R")
+setwd("/Users/helmut/Desktop/Simulations")   # set working directory path
 
+######################################################################################
+# Function for data simulation
+fct_datasim <- function(setup,dgp){
+  p <- 3
+  betaXY <- c(0.3,0.3,0.3)
+  cov <- matrix(c(1,0.3,0.3,1),2,2)
+  errors <-(rmvnorm(n,rep(0,2),cov))
+  X <- matrix(rnorm(n*p), n, p)
+  colnames(X) <- paste("Xvar", 1:p, sep="")
+
+  if(dgp==0) {          # no violations of A1 & A3; inequalities are binding
+    a <- 0; alpha =  rep(a, n)
+    g <- 0; gamma = rep(g, n)
+  } else if(dgp==1) {   # no violations of A1 & A3 but with heterogeneity; inequaltities are not binding (all tests are conservative)
+    a <- 0.2; alpha =  rep(a, n)
+    g <- 0;   gamma = rep(g, n)
+  } else if(dgp==2) {   # local violation of monotonicity
+    alpha <- as.numeric(ifelse(X[,1]< qnorm(0.15), -0.75, 0.4))
+    g <- 0; gamma = rep(g, n)
+  } else if(dgp==3) {   # local violation of exclusion restriction
+    a <- 0.2; alpha =  rep(a, n)
+    gamma <- as.numeric(ifelse(X[,2]< qnorm(0.15), 1.25, 0))
+  } else if(dgp==4) {   # global violation of exclusion restriction
+    a <- 0.2; alpha =  rep(a, n)
+    g <- 0.5; gamma = rep(g, n)
+  } else if(dgp==5) {   # global violation of exclusion restriction but with sign heterogeneity
+    a <- 0.2; alpha =  rep(a, n)
+    gamma <- as.numeric(ifelse(X[,3]< qnorm(0.5), 0.5, -0.5))
+  } else {
+    stop("invalid choice of dgp")
+  }
+
+  if (setup == 'A'){         # basic setup (randomized experiment)
+    Z <- rbinom(n, size=1, prob=0.5)
+    Dlatent <- alpha*Z+errors[,1]
+    D<-as.numeric( Dlatent>0 )
+  } else if (setup == 'B'){  # easy propensity and strong confounding
+    Zlatent = 0.2*X[,1] + 0.2*X[,2] + 0.2*X[,3] + rnorm(n)
+    Z = as.numeric( Zlatent>0 )
+    b = 0.5 * log(1 + exp(X[,1] + X[,2] + X[,3]))
+    Dlatent = b + alpha*Z + errors[,1]
+    D = as.numeric( Dlatent > 0 )
+  } else {
+    stop("invalid choice of setup")
+  }
+  Y <- as.vector(1*D+gamma*Z+X%*%betaXY+errors[,2])
+  data <- as.data.frame(cbind(Y,D,Z,X))
+  return(data)
+}
 ######################################################################################
 
 seed=1234
@@ -22,7 +69,7 @@ set.seed(seed)
 #####################
 setup <- "A"          # A: randomized experiment, B: easy confounding of Z and strong confounding of D
 n <- 3000             # number of observations
-R <- 8             # number of Monte Carlo replications
+R <- 100              # number of Monte Carlo replications
 
 subsets <- 4
 siglevel <- 0.05
@@ -32,7 +79,7 @@ siglevel <- 0.05
 system <- Sys.info()['sysname']
 if(system == 'Windows') {
   cl <- makeCluster(detectCores())
-  clusterEvalQ(cl, c(library(grf),library(rlearner),library(rpart),library(treeClust),library(mvtnorm)))
+  clusterEvalQ(cl, c(library(LATEtest),library(grf),library(rpart),library(treeClust),library(mvtnorm)))
 } else if(system == 'Darwin') {
   cl <- makeForkCluster(detectCores())
 }
@@ -49,7 +96,7 @@ print("--------------------------------------------")
 
 ##### Run simulation #####
 ##########################
-for (dgp in 0:5) {         #loop over all dgps; see dgp_setup.R
+for (dgp in 0:5) {         #loop over all dgps; see fct_datasim() above
   start_time = Sys.time()
   sim <- foreach(r=1:R, .combine=rbind) %dopar% {
     set.seed(r)
@@ -83,7 +130,7 @@ for (dgp in 0:5) {         #loop over all dgps; see dgp_setup.R
   print(table(sim$dim1))
   print("table(sim$dim2)")
   print(table(sim$dim2))
-  save.image(file=paste("FGKtest","_setup",setup,"_dgp",dgp,".Rdata",sep=""))
+  save.image(file=paste("FGK","_setup",setup,"_dgp",dgp,".Rdata",sep=""))
   print("--------------------------------------")
 }
 sink(file=NULL)
